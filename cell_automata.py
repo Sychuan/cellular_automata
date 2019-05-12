187c00a210a713c5cd7ce665d92aeb4547d7340e
Sychuan <macumoto@gmail.com>
2019-05-12 17:43:44 +0300

    update

--- cell_automata.py
+++ cell_automata.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-time = 500
-size = 500
+time = 600
+size = 600
+base = 5
 save_every_step = False
 
 # creating empty world x-axis = space size, y-axis time
@@ -11,41 +12,34 @@ cells = np.zeros((size, time), dtype=np.int32)
 
 # random starting configuration
 def random_start():
-    return np.random.randint(0, 3, size)
-
-
-# starting configuration
-
+    return np.random.randint(0, base, size)
 cells[:, 0] = random_start()
-
-configuration = np.array(
-    [[1, 1, 1], [1, 1, 0], [1, 0, 1], [1, 0, 0], [0, 1, 1], [0, 1, 0], [0, 0, 1], [0, 0, 0]]
-)
-
-configuration = np.array(
-    [[0, 0, 0], [0, 0, 1], [0, 0, 2], [0, 1, 0], [0, 1, 1], [0, 1, 2], [0, 2, 0], [0, 2, 1], [0, 2, 2],
-     [1, 0, 0], [1, 0, 1], [1, 0, 2], [1, 1, 0], [1, 1, 1], [1, 1, 2], [1, 2, 0], [1, 2, 1], [1, 2, 2], [2, 2, 2]]
-)[::-1]
 
 
 # Convert rule to number in Wolfram notation
-def rule_from_number(n):
-    base = 3
-    rule = np.zeros(19)
+def rule_from_number(n, base, length):
+    rule = np.zeros(length)
     i = -1
-    while n > 1:
+    while n > (base - 1):
         res = n % base
         n = n // base
         rule[i] = res
         i -= 1
     rule[i] = n
-    print(rule)
     return rule
 
 
+# create all possibpe position variations for given base
+configurations = []
+for k in range(base ** 3):
+    configurations.append(rule_from_number(k, base, 3))
+
+configurations = np.array(configurations[::-1])
+# print(configurations)
+print('number of possible rules: {}'.format(base ** (base ** 3)))
 
 def img_save(t, number, cells):
-    plt.figure(figsize=(4, 4), dpi=300)
+    plt.figure(figsize=(5, 5), dpi=300)
     plt.set_cmap('jet')
     ax = plt.axes([0, 0, 1, 1])
     plt.axis('off')
@@ -57,20 +51,19 @@ def img_save(t, number, cells):
 
 
 def automata_generate(rules, number):
-    print('starting')
+    print('starting rule'.format(rules))
     # save starting configuration
     if save_every_step:
         img_save(0, number, cells)
     # generate every step
     for t in range(1, time):
         left = np.roll(cells[:, t - 1], 1)
-        center = cells[:, t - 1]
         right = np.roll(cells[:, t - 1], -1)
         center = cells[:, t - 1]
         for n in range(size):
             # print(left[n], center[n], right[n])
             test = np.array([left[n], center[n], right[n]])
-            for k, rule in enumerate(configuration):
+            for k, rule in enumerate(configurations):
                 if all(rule == test):
                     cells[n, t] = rules[k]
                     break
@@ -78,13 +71,13 @@ def automata_generate(rules, number):
             img_save(t, number, cells)
     if not save_every_step:
         img_save(time, number, cells)
-
     print('finished')
 
 
-rule_numbers = [1768]  # np.arange(1, 2*3**7+2*3**6+2*3**5+2*3**4+2*3**3+2*3**2+2*3**1+2)
+# how many different rules to try?
+rule_numbers = [32984747330028382193832345567771234334459484371]  #np.arange(1, 200)
 
 for number in rule_numbers:
-    rules = rule_from_number(number)
+    rules = rule_from_number(number, base, base **3)
     automata_generate(rules, number)
 print('all finished')
